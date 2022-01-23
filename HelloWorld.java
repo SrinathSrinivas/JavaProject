0fcf74580ab9f68dece43679365d8c275375b92e
SrinathSrinivas <srinath.s@live.com>
2022-01-23 22:59:00 +0530

    Method 2 - 20220123-v3

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,12 +3,17 @@ public class HelloWorld {
         System.out.println("Hello World");
         newMethod();
         newMethod1();
+        newMethod2();
     }
     public static void newMethod(){
         System.out.println("Inside New Method");
     }
     public static void newMethod1(){
         System.out.println("Inside New Method1");
+        String s = "1";
+    }
+    public static void newMethod2(){
+        System.out.println("Inside New Method2");
         String s = "1";
     }
 }
