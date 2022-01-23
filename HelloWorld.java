be8c742446d5caa7181c2fc88f4aa636f097afd6
SrinathSrinivas <srinath.s@live.com>
2022-01-23 15:12:53 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+    public void newMethod(){
+        System.out.println("Inside New Method");
+    }
+}
