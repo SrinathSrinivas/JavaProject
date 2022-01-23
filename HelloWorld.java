7476df1790f6e28d40bca78c3578195d40b1c108
Srisha Srinivas <srisha.s@outlook.com>
2022-01-23 15:54:23 +0530

    Test Code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
+        newMethod();
+        newMethod1();
     }
-    public void newMethod(){
+    public static void newMethod(){
+        System.out.println("Inside New Method");
+    }
+    public static void newMethod1(){
         System.out.println("Inside New Method");
     }
 }
